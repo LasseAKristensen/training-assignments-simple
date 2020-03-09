27a02415d0ce890ba2668289fe5ab15df66aa005
Lasse <lk@lorc.dk>
2020-03-09 20:29:45 +0100

    Update BoardFactory.java

--- src/java/eu/sig/training/ch02/BoardFactory.java
+++ src/java/eu/sig/training/ch02/BoardFactory.java
@@ -7,8 +7,7 @@ public class BoardFactory {
 
         Board board = new Board(grid);
 
-        int width = board.getWidth();
-        int height = board.getHeight();
+        int width = board.getWidth(), height = board.getHeight();
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
                 Square square = grid[x][y];
