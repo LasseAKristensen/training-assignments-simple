98d990bff96c94acc33e0662ecfeef949447c608
Lasse <lk@lorc.dk>
2020-03-10 13:45:39 +0100

    Update BoardFactory.java

--- src/java/eu/sig/training/ch02/BoardFactory.java
+++ src/java/eu/sig/training/ch02/BoardFactory.java
@@ -3,26 +3,44 @@ package eu.sig.training.ch02;
 public class BoardFactory {
     // tag::createBoard[]
     public Board createBoard(Square[][] grid) {
-        assert grid != null;
+        return new BoardCreator(grid).create();
+    }
+    // end::createBoard[]
+}
 
-        Board board = new Board(grid);
-
-        int width = board.getWidth(), height = board.getHeight(); // LK: combining two statements in one line
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
+class BoardCreator{
+    
+    private Square[][] grid;
+    private Board board;
+    private int width;
+    private int height;
+    
+    BoardCreator(Square[][] grid){
+        assert grid!= null;
+        this.grid = grid;
+        this.board = new Board(grid);
+        this.width = board.getWidth();
+        this.height = board.getHeight();
+    }
+    
+    Board create(){
+        for (int x = 0; x < width; x++){
+            for (int y = 0; y < height; y++){
                 Square square = grid[x][y];
-                for (Direction dir : Direction.values()) {
-                    int dirX = (width + x + dir.getDeltaX()) % width;
-                    int dirY = (height + y + dir.getDeltaY()) % height;
-                    Square neighbour = grid[dirX][dirY];
-                    square.link(neighbour, dir);
+                for (Direction dir : Direction.values()){
+                    setLink(square, dir, x, y);
                 }
             }
         }
-
-        return board;
+        return this.board;
     }
-    // end::createBoard[]
+    
+    private void setLink(Square square, Direction dir, int x, int y){
+        int dirX = (width + x + dir.getDeltaX()) % width;
+        int dirY = (height + y + dir.getDeltaY()) % height;
+        Square neighbour = grid[dirX][dirY];
+        square.link(neighbour, dir); 
+    }  
 }
 
 class Board {
