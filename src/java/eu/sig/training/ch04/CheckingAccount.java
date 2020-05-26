dff408a81c0ceb3e9d0ddf522adddb0d630ecda8
Lasse <lk@lorc.dk>
2020-05-26 15:18:11 +0200

    Update CheckingAccount.java

--- src/java/eu/sig/training/ch04/CheckingAccount.java
+++ src/java/eu/sig/training/ch04/CheckingAccount.java
@@ -6,20 +6,13 @@ public class CheckingAccount {
     private Money balance = new Money();
     private int transferLimit = 100;
 
-    public Transfer makeTransfer(String counterAccount, Money amount)
-        throws BusinessException {
+    public Transfer makeTransfer(String counterAccount, Money amount) throws BusinessException {
         // 1. Check withdrawal limit:
         if (amount.greaterThan(this.transferLimit)) {
             throw new BusinessException("Limit exceeded!");
         }
         // 2. Assuming result is 9-digit bank account number, validate 11-test:
-        int sum = 0;
-        for (int i = 0; i < counterAccount.length(); i++) {
-            char character = counterAccount.charAt(i);
-            int characterValue = Character.getNumericValue(character);
-            sum = sum + (9 - i) * characterValue;
-        }
-        if (sum % 11 == 0) {
+        if (Accounts.isValid(counterAccount)) {
             // 3. Look up counter account and make transfer object:
             CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
             Transfer result = new Transfer(this, acct, amount);
