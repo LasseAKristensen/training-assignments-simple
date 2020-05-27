c3c57d8ecca42a9c277a2ba5c46a88d7e933b1b1
Lasse <lk@lorc.dk>
2020-05-27 15:09:21 +0200

    Update Account.java

--- src/java/eu/sig/training/ch04/Account.java
+++ src/java/eu/sig/training/ch04/Account.java
@@ -10,21 +10,22 @@ package eu.sig.training.ch04;
  * @author lk
  */
 public class Account {
-    
     public Transfer makeTransfer(String counterAccount, Money amount)
             throws BusinessException {
-        // 1. Assuming result is 9-digit bank account number, validate 11-test:
-        int sum = 0;
-        for (int i = 0; i < counterAccount.length(); i++){
-            sum = sum + (9 - i) * Character.getNumericValue(counterAccount.charAt(i));
-        }
-        if (sum % 11 == 0){
+        if (isValid(counterAccount)){
             // 2. look up counter account and make transfer object:
             CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
-            Transfer result = new Transfer(this, acct, amount);
-            return result;
+            return new Transfer(this, acct, amount);
         } else {
             throw new BusinessException("Invalid acount number");
         }
+    }
+    
+    public static boolean isValid(String number) {
+        int sum = 0;
+        for (int i = 0; i < number.length(); i++) {
+            sum = sum + (9 - i) * Character.getNumericValue(number.charAt(i));
+        }
+        return sum % 11 == 0;
     }
 }
