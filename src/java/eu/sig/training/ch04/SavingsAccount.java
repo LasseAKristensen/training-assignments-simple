35b95967ce04662c9ee225315310e69094fdb808
Lasse <lk@lorc.dk>
2020-05-27 09:07:36 +0200

    Update SavingsAccount.java

--- src/java/eu/sig/training/ch04/SavingsAccount.java
+++ src/java/eu/sig/training/ch04/SavingsAccount.java
@@ -1,28 +1,22 @@
 package eu.sig.training.ch04;
 
 // tag::SavingsAccount[]
-public class SavingsAccount {
+public class SavingsAccount extends Account{
     private static final float INTEREST_PERCENTAGE = 0.04f;
     private Money balance = new Money();
     private CheckingAccount registeredCounterAccount;
-
+    
+    @Override
     public Transfer makeTransfer(String counterAccount, Money amount) 
         throws BusinessException {
         // 1. Assuming result is 9-digit bank account number, validate 11-test:
-        if (Accounts.isValid(counterAccount)) {
-            // 2. Look up counter account and make transfer object:
-            CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
-            Transfer result = new Transfer(this, acct, amount); // <2>
-            // 3. Check whether withdrawal is to registered counter account:
-            if (result.getCounterAccount().equals(this.registeredCounterAccount)) 
-            {
-                return result;
-            } else {
-                throw new BusinessException("Counter-account not registered!");
-            }
+        Transfer result = super.makeTransfer(counterAccount, amount); // <2>
+        // 3. Check whether withdrawal is to registered counter account:
+        if (result.getCounterAccount().equals(this.registeredCounterAccount)) {
+            return result;
         } else {
-            throw new BusinessException("Invalid account number!!");
-        }
+            throw new BusinessException("Counter-account not registered!");
+        } 
     }
 
     public void addInterest() {
