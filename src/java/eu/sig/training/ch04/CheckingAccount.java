3c0363b7107a54348cf8b2ebf74655326232b66c
Lasse <lk@lorc.dk>
2020-05-27 09:07:32 +0200

    Update CheckingAccount.java

--- src/java/eu/sig/training/ch04/CheckingAccount.java
+++ src/java/eu/sig/training/ch04/CheckingAccount.java
@@ -1,25 +1,19 @@
 package eu.sig.training.ch04;
 
 // tag::CheckingAccount[]
-public class CheckingAccount {
+public class CheckingAccount extends Account {
     private static final float INTEREST_PERCENTAGE = 0.01f;
     private Money balance = new Money();
     private int transferLimit = 100;
-
-    public Transfer makeTransfer(String counterAccount, Money amount) throws BusinessException {
+    
+    @Override
+    public Transfer makeTransfer(String counterAccount, Money amount) 
+            throws BusinessException {
         // 1. Check withdrawal limit:
         if (amount.greaterThan(this.transferLimit)) {
             throw new BusinessException("Limit exceeded!");
         }
-        // 2. Assuming result is 9-digit bank account number, validate 11-test:
-        if (Accounts.isValid(counterAccount)) {
-            // 3. Look up counter account and make transfer object:
-            CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
-            Transfer result = new Transfer(this, acct, amount);
-            return result;
-        } else {
-            throw new BusinessException("Invalid account number!");
-        }
+        return super.makeTransfer(counterAccount, amount);
     }
 
     public void addInterest() {
